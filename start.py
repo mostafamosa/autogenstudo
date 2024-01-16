246add704bd8737296bc0e636df21613f2971a0f
mostafamosa <mostafa.mosa3@gmail.com>
2024-01-16 19:02:25 +0100

    Update start.py

--- start.py
+++ start.py
@@ -5,8 +5,7 @@ import subprocess
 os.environ["OPENAI_API_KEY"] = "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"
 
 
-# Start your application
-autogenstudio ui --port 8081
+
 
 # Start your application
 subprocess.call(["autogenstudio", "ui", "--port", "8081"])
