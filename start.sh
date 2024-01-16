070eb0faa235727dd5bad27fc34f9992214fba87
mostafamosa <mostafa.mosa3@gmail.com>
2024-01-16 18:13:58 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,3 @@
-#!/bin/bash
-# Set default values if not provided
-DOMAIN=${RAILWAY_PUBLIC_DOMAIN:-"up.railway.app"}
-GRAPHQL_PATH=${GRAPHQL_PATH:-"/v1/gql"}
-GRAPHQL_ENDPOINT="https://${DOMAIN}${GRAPHQL_PATH}"
-
-# Export for application use
-export GRAPHQL_ENDPOINT
 
 # Start your application
 autogenstudio ui --port 8081
